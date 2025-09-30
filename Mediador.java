8eb07929f0e9729ce160bd17af05e176f5691c45
farentsen999 <franciscoarentsenalister@gmail.com>
2025-09-30 18:42:26 -0300

    Se añadio la clase Mediador al repositorio

---
+++ Mediador.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Mediador {
+    private BaseDatos baseDatos;
+    public Mediador(){
+    }
+
+    public void hacerMatch(){
+        /*
+        Cada x segundos intenta hacer match con los objetos reportados como perdidos y encontrados
+        si encuentra una o mas coincidencias cambia el estado del reporte a encontrado y
+        notifica al usuario
+         */
+    }
+
+    // cada cierto tiempo revisa base de datos, si encuentra uno o mas match cambiar el estado a encontrado y notificar al usuario
+
+
+}
